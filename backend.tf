d65a12f41a826a4b7fec97aebe6512a684defe9f
Williamson Fru Aboringong <fru.williams@gmail.com>
2024-09-02 12:54:59 -0400

    update  workspace name Lap 3

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   required_version = ">= 1.1.0"
   backend "remote" {
-    # organization = "CYPRO-TECH"
+    organization = "CYPRO-TECH"
     workspaces {
       name = "ace-iac-day-two"
     }
